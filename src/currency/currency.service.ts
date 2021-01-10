0ba5be19cfca9669c35659a4bf05da7ca1eda186
Lucas Bastos <lucas.b@adireto.com>
2021-01-10 20:39:28 -0300

    add return to function getCurrency

--- src/currency/currency.service.ts
+++ src/currency/currency.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CurrencyService {
-  async getCurrency(currency: string) {
+  async getCurrency(currency: string): Promise<any> {
 
   }
 }
