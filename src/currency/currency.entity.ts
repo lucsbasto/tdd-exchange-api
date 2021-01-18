2ff7e7f1cdae3db16395faa2760ea08877639ff0
Lucas Bastos <lucas.b@adireto.com>
2021-01-18 14:31:13 -0300

    feat: create currency entity

--- src/currency/currency.entity.ts
+++ src/currency/currency.entity.ts
@@ -1,4 +1,20 @@
+import { Column, CreateDateColumn, Entity, ObjectIdColumn, PrimaryColumn, Unique, UpdateDateColumn } from "typeorm";
+
+@Entity()
+@Unique(['currency'])
 export class Currency {
+  @ObjectIdColumn()
+  _id: string;
+
+  @PrimaryColumn()
   currency: string;
+
+  @Column()
   value: number;
+
+  @CreateDateColumn({type: 'timestamp with local time zone'})
+  createdAt: Date;
+
+  @UpdateDateColumn({type: 'timestamp with local time zone'})
+  updatedAt: Date;
 }
